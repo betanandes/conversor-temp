1e8b7f9ec2bedba48fb40ebbbe0db99ef26f2045
betanandes <robertafernandes46@gmail.com>
2025-05-03 21:34:12 -0300

    conversor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  selector: 'app-conversor',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  inputValue: number | null = null;
+  result: number | null = null;
+  convertToCelsius = false;
+
+  toggleConversion() {
+    this.convertToCelsius = !this.convertToCelsius;
+    this.result = null;
+    this.inputValue = null;
+  }
+
+  converter() {
+    if (this.inputValue === null) return;
+
+    if (this.convertToCelsius) {
+      this.result = ((this.inputValue - 32) * 5) / 9;
+    } else {
+      this.result = (this.inputValue * 9) / 5 + 32;
+    }
+  }
 }
